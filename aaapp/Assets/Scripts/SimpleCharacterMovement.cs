19905347063d4682776284be0568f855c887befb
Patrick Schmid <schmidpatrick@outlook.com>
2018-11-13 18:42:11 +0100

    Add and test some sound things.

--- aaapp/Assets/Scripts/SimpleCharacterMovement.cs
+++ aaapp/Assets/Scripts/SimpleCharacterMovement.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 
 public class SimpleCharacterMovement : MonoBehaviour
 {
-
-    // The GameObject is made to bounce using the space key.
-    // Also the GameOject can be moved forward/backward and left/right.
-    // Add a Quad to the scene so this GameObject can collider with a floor.
-
-
     public float speed = 6.0f;
 	public float rotSpeed = 6.0f;
     public float jumpSpeed = 8.0f;
@@ -32,9 +26,6 @@ public class SimpleCharacterMovement : MonoBehaviour
 
         if (controller.isGrounded)
         {
-            // We are grounded, so recalculate
-            // move direction directly from axes
-
             moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection = moveDirection * speed;
@@ -54,6 +45,5 @@ public class SimpleCharacterMovement : MonoBehaviour
 
         // Move the controller
         controller.Move(moveDirection * Time.deltaTime);
-
     }
 }
