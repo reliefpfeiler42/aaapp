98da4cb1a15499ce75843f4d379cf2f10e26a54b
Patrick Schmid <schmidpatrick@outlook.com>
2018-12-28 11:48:03 +0100

    First steps on image recognition

---
+++ aaapp/Assets/AAApp/Scripts/AugmentedImageController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using GoogleARCore;
+using UnityEngine;
+
+public class AugmentedImageController : MonoBehaviour
+{
+
+	public GameObject PrefabToInstantiate;
+    private readonly List<AugmentedImage> _images = new List<AugmentedImage>();
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+		if (Session.Status != SessionStatus.Tracking)
+		{
+			return;
+		}
+
+		Session.GetTrackables(_images, TrackableQueryFilter.Updated);
+		VisualizeTrackables();
+    }
+
+	private void VisualizeTrackables()
+	{
+		foreach (var image in _images)
+		{
+			if (image.DatabaseIndex == 0)
+			{
+				Anchor anchor = image.CreateAnchor(image.CenterPose);
+				var myGameObject = Instantiate(PrefabToInstantiate, anchor.transform);
+			}
+		}
+	}
+
+}
