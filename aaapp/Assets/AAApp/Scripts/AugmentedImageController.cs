d4b526e74a5232354102647c422c7c190fb6d342
Patrick Schmid <schmidpatrick@outlook.com>
2019-01-20 09:21:15 +0100

    Add BusterDrone and instantiate in the back of the camera

--- aaapp/Assets/AAApp/Scripts/AugmentedImageController.cs
+++ aaapp/Assets/AAApp/Scripts/AugmentedImageController.cs
@@ -7,12 +7,16 @@ using UnityEngine;
 public class AugmentedImageController : MonoBehaviour
 {
 	public GameObject PrefabDino;
+	public GameObject PrefabDrone;
+	public GameObject Camera;
     private readonly List<AugmentedImage> _images = new List<AugmentedImage>();
 	private bool _dinoIsActive;
+	private bool _droneIsActive;
 
     void Start()
     {
 		_dinoIsActive = false;
+		_droneIsActive = false;
     }
 
     void Update()
@@ -30,12 +34,29 @@ public class AugmentedImageController : MonoBehaviour
 	{
 		foreach (var image in _images)
 		{
-			if (image.DatabaseIndex == 0 && _dinoIsActive == false)
+			// if (image.DatabaseIndex == 0 && _dinoIsActive == false)
+			// {
+			// 	Anchor anchor = image.CreateAnchor(image.CenterPose);
+			// 	var dino = Instantiate(PrefabDino, anchor.transform);
+			// 	_dinoIsActive = true;
+			// 	dino.transform.localScale = new Vector3(image.ExtentX, image.ExtentX, image.ExtentX);
+			// }
+			if (image.DatabaseIndex == 0 && _droneIsActive == false)
 			{
+				Vector3 offset = new Vector3(0, 0, 2);
+				float imageFactor = 2;
 				Anchor anchor = image.CreateAnchor(image.CenterPose);
-				var dino = Instantiate(PrefabDino, anchor.transform);
-				_dinoIsActive = true;
-				dino.transform.localScale = new Vector3(image.ExtentX, image.ExtentX, image.ExtentX);
+				// anchor.transform.position = Camera.transform.position + offset;
+				GameObject drone = Instantiate(PrefabDrone, anchor.transform);
+				_droneIsActive = true;
+				drone.transform.localScale = new Vector3(image.ExtentX * imageFactor, image.ExtentX *imageFactor, image.ExtentX * imageFactor);
+				drone.transform.Rotate(90f, 0, 0);
+				drone.transform.position = Camera.transform.position + offset;
+
+			}
+			else
+			{
+				return;
 			}
 		}
 	}
