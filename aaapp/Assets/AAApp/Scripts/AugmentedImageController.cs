3d5eae8d60fafce36c4485b7d75446340a0d55eb
Patrick Schmid <schmidpatrick@outlook.com>
2019-03-06 11:13:48 +0100

    Cleanup and add Scenes for user test

--- aaapp/Assets/AAApp/Scripts/AugmentedImageController.cs
+++ aaapp/Assets/AAApp/Scripts/AugmentedImageController.cs
@@ -6,59 +6,48 @@ using UnityEngine;
 
 public class AugmentedImageController : MonoBehaviour
 {
-	public GameObject PrefabDino;
-	public GameObject PrefabDrone;
-	public GameObject Camera;
+    public GameObject DinoPrefab;
+    // public GameObject DronePrefab;
     private readonly List<AugmentedImage> _images = new List<AugmentedImage>();
-	private bool _dinoIsActive;
-	private bool _droneIsActive;
+    private bool _dinoIsActive;
+    // private bool _droneIsActive;
 
     void Start()
     {
-		_dinoIsActive = false;
-		_droneIsActive = false;
+        _dinoIsActive = false;
+        // _droneIsActive = false;
     }
 
     void Update()
     {
-		if (Session.Status != SessionStatus.Tracking)
-		{
-			return;
-		}
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            // Debug.Log("#####################Lost Tracking#######################");
+            return;
+        }
 
-		Session.GetTrackables(_images, TrackableQueryFilter.Updated);
-		VisualizeGameObject();
+        Session.GetTrackables(_images, TrackableQueryFilter.Updated);
+        VisualizeGameObject();
     }
 
-	private void VisualizeGameObject()
-	{
-		foreach (var image in _images)
-		{
-			// if (image.DatabaseIndex == 0 && _dinoIsActive == false)
-			// {
-			// 	Anchor anchor = image.CreateAnchor(image.CenterPose);
-			// 	var dino = Instantiate(PrefabDino, anchor.transform);
-			// 	_dinoIsActive = true;
-			// 	dino.transform.localScale = new Vector3(image.ExtentX, image.ExtentX, image.ExtentX);
-			// }
-			if (image.DatabaseIndex == 0 && _droneIsActive == false)
-			{
-				Vector3 offset = new Vector3(0, 0, 2);
-				float imageFactor = 2;
-				Anchor anchor = image.CreateAnchor(image.CenterPose);
-				// anchor.transform.position = Camera.transform.position + offset;
-				GameObject drone = Instantiate(PrefabDrone, anchor.transform);
-				_droneIsActive = true;
-				drone.transform.localScale = new Vector3(image.ExtentX * imageFactor, image.ExtentX *imageFactor, image.ExtentX * imageFactor);
-				drone.transform.Rotate(90f, 0, 0);
-				drone.transform.position = Camera.transform.position + offset;
-
-			}
-			else
-			{
-				return;
-			}
-		}
-	}
+    private void VisualizeGameObject()
+    {
+        foreach (var image in _images)
+        {
+            if (image.DatabaseIndex == 0 && _dinoIsActive == false)
+            {
+                Anchor anchor = image.CreateAnchor(image.CenterPose);
+                var dino = Instantiate(DinoPrefab, anchor.transform);
+                _dinoIsActive = true;
+            }
+            // if (image.DatabaseIndex == 1 && _droneIsActive == false)
+            // {
+            //     Vector3 offset = new Vector3(0, 0, 10);
+            //     Anchor anchor = image.CreateAnchor(image.CenterPose);
+            //     GameObject drone = Instantiate(DronePrefab, anchor.transform);
+            //     _droneIsActive = true;
+            // }
+        }
+    }
 
 }
